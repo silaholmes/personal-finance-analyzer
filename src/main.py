9e48b77f7631df53730c5af9ba5468e3a9bda8ec
Aalaa Ayesh <aalaaayesh@gmail.com>
2026-01-17 20:43:47 +0200

    Initial project structure

---
+++ src/main.py
@@ -0,0 +1,17 @@
+"""
+Personal Finance Analyzer
+Author: [Alaa Ayesh]
+Date: January 2, 2026
+"""
+
+li = ["cvv",7,"jjk"]
+print(" ".join(map(str,li)))
+
+# import pandas as pd
+# df = pd.read_csv("Account statement.csv")
+# def main():
+#     print("Starting my ML Engineering journey!")
+#     print("Day 1: Building a finance analyzer")
+
+# if __name__ == "__main__":
+#     main()
